625ba4c2aef656544bd3c33f665ffbf0c20c41f8
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-02-09 12:01:43 +0100

    opsaetning

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-slate-400 hover:text-sky-400">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
