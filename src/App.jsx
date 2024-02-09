7b1a9f663999ef6bfcd88eea2263b2258c28ba12
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-02-09 12:39:47 +0100

    partials added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.scss'
+import { Footer } from './Components/Partials/Footer/Footer'
+import { Header } from './Components/Partials/Header/Header'
+import { Main } from './Components/Partials/Main/Main'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-slate-400 hover:text-sky-400">
-        Hello world!
-      </h1>
+      <Header />
+      <Main>
+        <div>
+          <h1 className="text-3xl font-bold underline text-slate-400 hover:text-sky-400">
+            Hello world!
+          </h1>
+        </div>
+      </Main>
+      <Footer />
     </>
   )
 }
---
+++ src/Components/Partials/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import { NavLink } from "react-router-dom"
+
+export const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><NavLink>Home</NavLink></li>
+            <li><NavLink>About</NavLink></li>
+            <li><NavLink>Home</NavLink></li>
+            <li><NavLink>Home</NavLink></li>
+        </ul>
+    </nav>
+  )
+}
